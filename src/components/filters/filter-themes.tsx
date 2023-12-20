3e8f49661c1e30966922270d9544f0aa3556e3b5
sahara.ap <sahara.ap13@gmail.com>
2023-12-20 20:51:30 +0300

    fix. Запятые

--- src/components/filters/filter-themes.tsx
+++ src/components/filters/filter-themes.tsx
@@ -1,9 +1,47 @@
 import { useState } from 'react';
-import { FILTER_THEMES } from '../../consts';
 import { TFilterThemes } from '../../types/types';
 
 function FilterThemes(): JSX.Element {
   const [selectedFilterId, setSelectedFilterId] = useState('all');
+
+  const filterThemes: TFilterThemes[] = [
+    {
+      title: 'Все квесты',
+      logo: '#icon-all-quests',
+      logoWidth: '26',
+      id: 'all',
+    },
+    {
+      title: 'Приключения',
+      logo: '#icon-adventure',
+      logoWidth: '36',
+      id: 'adventure',
+    },
+    {
+      title: 'Ужасы',
+      logo: '#icon-horror',
+      logoWidth: '30',
+      id: 'horror',
+    },
+    {
+      title: 'Мистика',
+      logo: '#icon-mystic',
+      logoWidth: '30',
+      id: 'mystic',
+    },
+    {
+      title: 'Детектив',
+      logo: '#icon-detective',
+      logoWidth: '40',
+      id: 'detective',
+    },
+    {
+      title: 'Sci-fi',
+      logo: '#icon-sci-fi',
+      logoWidth: '28',
+      id: 'sciFi',
+    },
+  ];
 
   function handleFilterClick(filterId: TFilterThemes['id']) {
     setSelectedFilterId(filterId);
@@ -13,7 +51,7 @@ function FilterThemes(): JSX.Element {
     <fieldset className="filter__section">
       <legend className="visually-hidden">Тематика</legend>
       <ul className="filter__list">
-        {FILTER_THEMES.map((item) => (
+        {filterThemes.map((item) => (
           <li key={item.id} className="filter__item">
             <input
               type="radio"
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,3 @@
-import { TFilterThemes } from './types/types';
-
 enum AppRoute {
   Booking = '/booking',
   Contacts = '/contacts',
@@ -16,47 +14,7 @@ enum AuthStatus {
   Unknown = 'UNKNOWN'
 }
 
-const FILTER_THEMES: TFilterThemes[] = [
-  {
-    title: 'Все квесты',
-    logo: '#icon-all-quests',
-    logoWidth: '26',
-    id: 'all',
-  },
-  {
-    title: 'Приключения',
-    logo: '#icon-adventure',
-    logoWidth: '36',
-    id: 'adventure',
-  },
-  {
-    title: 'Ужасы',
-    logo: '#icon-horror',
-    logoWidth: '30',
-    id: 'horror',
-  },
-  {
-    title: 'Мистика',
-    logo: '#icon-mystic',
-    logoWidth: '30',
-    id: 'mystic',
-  },
-  {
-    title: 'Детектив',
-    logo: '#icon-detective',
-    logoWidth: '40',
-    id: 'detective',
-  },
-  {
-    title: 'Sci-fi',
-    logo: '#icon-sci-fi',
-    logoWidth: '28',
-    id: 'sciFi',
-  },
-];
-
 export {
   AppRoute,
   AuthStatus,
-  FILTER_THEMES,
 };
