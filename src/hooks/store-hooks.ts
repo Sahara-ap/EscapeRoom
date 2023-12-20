a41a314b60becfb09ef43d3c5111bd2688bdc1bd
sahara.ap <sahara.ap13@gmail.com>
2023-12-20 19:07:05 +0300

    Типизация Store

---
+++ src/hooks/store-hooks.ts
@@ -0,0 +1,10 @@
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, State } from '../types/store';
+
+const useAppDispatch = () => useDispatch<AppDispatch>();
+const useAppSelector: TypedUseSelectorHook<State> = useSelector;
+
+export {
+  useAppDispatch,
+  useAppSelector,
+};
--- src/types/store.ts
+++ src/types/store.ts
@@ -1,8 +1,11 @@
 import { store } from '../store';
 
 type State = ReturnType<typeof store.getState>
+type AppDispatch = typeof store.dispatch
+
 
 export type {
   State,
+  AppDispatch,
 };
 
