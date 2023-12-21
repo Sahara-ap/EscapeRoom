291c359d6160feda25682a3afdcda432aab45e28
sahara.ap <sahara.ap13@gmail.com>
2023-12-21 11:36:01 +0300

    WIP. Карта. Настройки стилей перенесены в компонент

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,18 +1,22 @@
 import 'leaflet/dist/leaflet.css';
-import './map.css';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 
 function Map() {
   return (
 
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer
+      center={[59.96831, 30.31748]}
+      zoom={15}
+      scrollWheelZoom={false}
+      style={{ height: '100%', width: '100%' }}
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
         url='https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png'
       />
-      <Marker position={[51.505, -0.09]}>
+      <Marker position={[59.96831, 30.31748]}>
         <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
+          Escape Room <br /> .
         </Popup>
       </Marker>
     </MapContainer>
